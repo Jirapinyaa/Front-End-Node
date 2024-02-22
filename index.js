216604b91f34b251a34001cde1b4c3d4240ddb60
Jirapinyaa <s6506021622191@email.kmutnb.ac.th>
2024-02-22 15:23:14 +0700

    add

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 var bodyParser = require('body-parser')
 const path = require("path")
 
-//const base_url = "http://10.104.17.35"
-const base_url = "http://node57336-daienv-8730375.proen.app.ruk-com.cloud"
+const base_url = "http://10.104.17.35"
+//const base_url = "http://node57336-daienv-8730375.proen.app.ruk-com.cloud"
 //const base_url = "http://localhost:3000"
 
 app.set("views",path.join(__dirname,"/public/views"))
